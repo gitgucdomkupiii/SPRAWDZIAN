3dc8bdbaf6c2603e05203ca2be483f3fda5c08ec
gitgucdomkupiii <124463234+gitgucdomkupiii@users.noreply.github.com>
2023-02-21 15:49:52 +0100

    Add files via upload

---
+++ skrypt.js
@@ -0,0 +1,14 @@
+function oblicz(){
+    let a1=parseInt(document.getElementById("liczba").value);
+    let Fn=0;
+    if(n<=2){
+        Fn=an;
+    }else{
+        let (an-a1);
+        let (n-1);
+        while(i<=n){
+            (an-a1)/(n-1);
+        }
+    }
+    document.getElementById("wynik").value=Fn;
+    }
